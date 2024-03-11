1e18b3b0e111a7e6f533486306f27ab8da301ae1
AnaS <104474970+anamschiavo@users.noreply.github.com>
2024-03-11 19:02:17 -0300

    Trunquei as sobrevivências NaCl e adicionei fitting no UV-C (D4.30.R2A-3 terei que re irradiar)

--- Diamantina/NaCl/NaCl-escolhidos.R
+++ Diamantina/NaCl/NaCl-escolhidos.R
@@ -14,6 +14,8 @@ install.packages('scales')    # Pacote para transformação em log10 do eixo y d
 library(scales)
 install.packages('dplyr')
 library(dplyr)
+install.packages('ggtext')
+library(ggtext)
 
 #********** FUNÇÕES **********
 # Função para média corrigida
@@ -47,6 +49,7 @@ log10_minor_break = function (...){
   }
 }
 
+
 #********** LEITURA **********
 # Lê tabela em tsv, ou seja, separado por tab, vírgula como decimal, primeira linha é cabeçalho, preenche espáços vazios com NA
 nacltab <- read.table("NaCltab.tsv", sep="\t", dec=",", quote="", header=TRUE, fill=TRUE)
@@ -70,17 +73,19 @@ tab0 <- filter(tab, rep1$Conc=='0')
 media0 <- rep(tab0$media_rep, each=7)
 erro0 <- rep(tab0$erro_rep, each=7)
 sob <- media_rep/media0
+x <- trunc(sob*10^6)/10^6
 erro_sob <- sqrt((sob^2) * (((erro_rep / media_rep)^2) + ((erro0 / media0)^2)))
 
-tabgraf <- cbind.data.frame(rep1$Isolado, rep1$Conc, sob, erro_sob)
+tabgraf <- cbind.data.frame(rep1$Isolado, rep1$Conc, x, erro_sob)
 colnames(tabgraf) <- c('isolado', 'conc', 'sob', 'erro')
-
+tabgraf$isolado <- factor(tabgraf$isolado, labels=c("D14.15.010-3", "D14.15.100-1", "D14.30.010-1", "D14.30.010-5", "D20.15.010-12", "D20.30.010-2", "D21.30.010-2", "D23.15.010-1", "D23.30.010-1", "D23.30.010-3", "D4.15.R2A-5", "D4.30.100-3", "D4.30.R2A-2", "D4.30.R2A-4", "D5.30.010-1", "D7.15.010-6", "D7.15.R2A-6", "D7.30.100-9", "D8.15.R2A-3", "italic('E. coli')*textstyle(' MG1655')"))
+tabgraf$isolado <- factor(tabgraf$isolado)
 #********** GRÁFICOS **********
 graf_sob<- ggplot(tabgraf, aes(x=conc, y=sob)) +
     geom_point(size=.65)+   # Coloca as bolinhas
     geom_line(linewidth=.35)+   # Coloca as linhas
     geom_errorbar(aes(ymin=sob-erro, ymax=sob+erro), width=0.15, linewidth=.5)+    # Coloca as barras de erro
-    facet_wrap(~isolado, ncol=4)+    # Deixa facetado por espécie
+    facet_wrap(~isolado, ncol=4, labeller=label_parsed)+    # Deixa facetado por espécie
     theme_bw()+   # Muda a aparência para o tema certo
     xlab(bquote("[NaCl]"~(mol.L^-1)))+    # Coloca o nome do eixo x
     ylab(bquote("Sobrevivência"~(N/N[0])))+    # Coloca o nome do eixo y
@@ -95,59 +100,3 @@ graf_sob<- ggplot(tabgraf, aes(x=conc, y=sob)) +
 graf_sob
 
 ggsave('escolhidos_NaCl.png', graf_sob, device='png', unit='cm', width = 18, height = 25, dpi=300)   # Salva gráfico que acabamos de criar como um arquivo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#********** PRÉ-PROCESSAMENTO **********
-ufc <- as.matrix(nacltab[,c(3,4,5)])    # Cria matriz com as contagens das 3 gotinhas
-nome <- nacltab[,1]
-dose <- nacltab[,2]   # Cria um vetor com doses de UVC dadas
-media <- rowMeans(ufc, na.rm=TRUE)    # Tira média das gotinhas
-media_cor <- media*nacltab[,6]*100    # Faz média corrigida pegando média, multiplicando pelo fator de diluição e por 100 para ser UFC/mL
-desv_pad <- rowSds(ufc, na.rm=TRUE)   # Calcula o desvio padrão das três gotinhas
-n <- rowSums(!is.na(ufc))   # Calcula quantas gotinhas tem (3 ou 2)
-erro_pad <- desv_pad/(sqrt(n))    # Calcula erro padrão que, por definição, é desvio padrão sobre raiz quadrada do n
-erro_media <- ((nacltab[,6]*100))*(erro_pad)    # Erro associado as médias corrigidas. Propagação do erro considerando normal.
-media_cor_0 <- rep(c(media_cor[1], media_cor[8], media_cor[15], media_cor[22], media_cor[29], media_cor[36], media_cor[43], media_cor[50], media_cor[57], media_cor[64], media_cor[71]), each=7)    # Apenas os valores docontrole repetidos para fazer a sobrevivência
-erro_media_0 <- rep(c(erro_media[1], erro_media[8], erro_media[15], erro_media[22], erro_media[29], erro_media[36], erro_media[43], erro_media[50], erro_media[57], erro_media[64], erro_media[71]), each=7)   # Apenas valores do erro associados aos controles para fazer o erro das sobrevivências
-sobrev <- media_cor/media_cor_0   # Calcula sobrevivência (experimento sobre controle)
-sobrev <- trunc(sobrev*10^5)/10^5   # Trunca valos na 4a casa decimal
-erro_sobrev <- sqrt((sobrev^2)*(((erro_media/media_cor)^2)+((erro_media_0/media_cor_0)^2)))   # Calcula erro associado a sobrevivência com as fórmulas de propagação de erro
-graftab <- cbind.data.frame(nome, dose, sobrev, erro_sobrev)   # Cria tabela para fazer os gráficos, com espécie, doses, sobrevivência e erro associado a sobrevivência
-
-#********** GRÁFICOS **********
-graf_sob_facet<- ggplot(graftab, aes(x=dose, y=sobrev, color=nome)) +
-    geom_point(size=1)+   # Coloca as bolinhas
-    geom_line(linewidth=.7)+   # Coloca as linhas
-    geom_errorbar(aes(ymin=sobrev-erro_sobrev, ymax=sobrev+erro_sobrev), width=0.15, size=.5)+    # Coloca as barras de erro
-    facet_wrap(~nome)+    # Deixa facetado por espécie
-    theme_bw()+   # Muda a aparência para o tema certo
-    xlab("[NaCl] (mol/L)")+    # Coloca o nome do eixo x
-    ylab("Survival")+    # Coloca o nome do eixo y
-    theme(
-      panel.grid.major = element_line(size=.5),   # Define largura da linha maior do grid interno no gráfico
-      panel.grid.minor = element_line(size=.2),   # Define largura da linha menor do grid interno do gráfico
-      legend.position='none')+    # Retira a legenda
-    scale_y_log10(limits = c(1e-5,10),   # Transforma o eixo y em log10 com limiter de 1 a 10^-5
-                  labels = trans_format("log10", math_format(10^.x)),
-                   breaks = trans_breaks("log10", function(x) 10^x, n=5))
-
-ggsave('facetado.png')    # Salva gráfico que acabamos de criar como um arquivo
--- Diamantina/UV/UV-escolhidos.R
+++ Diamantina/UV/UV-escolhidos.R
@@ -1,4 +1,4 @@
-#Script Iniciação Científica Sabrina Pinheiro
+#Script Doutorado Ana Paula Muche Schiavo
 
 #REGRAS DE ENTRADA:
 #*************ORDEM DAS COLUNAS: 1. Espécie | 2. Dose | 3-5. UFC | 6. Fator de Diluição **********************************
@@ -64,8 +64,8 @@ rep1 <- filter(tab_media, Replicata=='1')
 rep2 <- filter(tab_media, Replicata=='2')
 rep3 <- filter(tab_media, Replicata=='3')
 
-media_rep <- rowMeans(cbind(rep1$mc, rep2$mc, rep2$mc), na.rm = TRUE)
-erro_rep <- sqrt(rowSums(cbind((1/3*rep1$erro_mc)^2, (1/3*rep2$erro_mc)^2, (1/3*rep1$erro_mc)^2), na.rm = TRUE))
+media_rep <- rowMeans(cbind(rep1$mc, rep2$mc, rep3$mc), na.rm = TRUE)
+erro_rep <- sqrt(rowSums(cbind((1/3*rep1$erro_mc)^2, (1/3*rep2$erro_mc)^2, (1/3*rep3$erro_mc)^2), na.rm = TRUE))
 
 tab <- cbind.data.frame(rep1$Isolado, rep1$Dose, media_rep, erro_rep)
 tab0 <- filter(tab, rep1$Dose=='0')
@@ -97,3 +97,149 @@ graf_sob<- ggplot(tabgraf, aes(x=Dose, y=sob)) +
 graf_sob
 
 ggsave('escolhidos_UV.png', graf_sob, device='png', unit='cm', width = 15, height = 20, dpi=300)   # Salva gráfico que acabamos de criar como um arquivo
+
+#********** FITTING **********
+x <- c(0, 50, 100, 200, 300, 500, 700, 1000)
+ld10 <- c()
+se_ld10 <- c()
+isolado <- c()
+
+# Isolado D16a.30.010-2
+d16a_30_010_2 <- filter(tabgraf, isolado=='D16a.30.010-2')
+y_16a_010_2 <- d16a_30_010_2$sob
+f <- 0
+fit_16a_010_2 <- nls(y_16a_010_2 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 100, n = 2))
+summary(fit_16a_010_2)
+ld10 <- append(ld10, summary(fit_16a_010_2)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_16a_010_2)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D16a.30.010-2', after=length(isolado))
+
+# Isolado D16a.30.010-7
+d16a_30_010_7 <- filter(tabgraf, isolado=='D16a.30.010-7')
+y_16a_010_7 <- d16a_30_010_7$sob
+fit_16a_010_7 <- nls(y_16a_010_7 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 120, n = 2))
+summary(fit_16a_010_7)
+ld10 <- append(ld10, summary(fit_16a_010_7)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_16a_010_7)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D16a.30.010-7', after=length(isolado))
+
+# Isolado D17b.30.010-2
+d17b_30_010_2 <- filter(tabgraf, isolado=='D17b.30.010-2')
+y_17b_010_2 <- d17b_30_010_2$sob
+fit_17b_010_2 <- nls(y_17b_010_2 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 200, n = 2))
+summary(fit_17b_010_2)
+ld10 <- append(ld10, summary(fit_17b_010_2)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_17b_010_2)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D17b.30.010-2', after=length(isolado))
+
+# Isolado D17b.30.010-4
+d17b_30_010_4 <- filter(tabgraf, isolado=='D17b.30.010-4')
+y_17b_010_4 <- d17b_30_010_4$sob
+fit_17b_010_4 <- nls(y_17b_010_4 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 200, n = 2))
+summary(fit_17b_010_4)
+ld10 <- append(ld10, summary(fit_17b_010_4)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_17b_010_4)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D17b.30.010-4', after=length(isolado))
+
+# Isolado D17b.30.010-4.2
+d17b_30_010_42 <- filter(tabgraf, isolado=='D17b.30.010-4.2')
+y_17b_010_42 <- d17b_30_010_42$sob
+fit_17b_010_42 <- nls(y_17b_010_42 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 200, n = 2))
+summary(fit_17b_010_42)
+ld10 <- append(ld10, summary(fit_17b_010_42)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_17b_010_42)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D17b.30.010-4.2', after=length(isolado))
+
+# Isolado D17b.30.010-5
+d17b_30_010_5 <- filter(tabgraf, isolado=='D17b.30.010-5')
+y_17b_010_5 <- d17b_30_010_5$sob
+fit_17b_010_5 <- nls(y_17b_010_5 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 150, n = 2))
+summary(fit_17b_010_5)
+ld10 <- append(ld10, summary(fit_17b_010_5)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_17b_010_5)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D17b.30.010-5', after=length(isolado))
+
+# Isolado D17b.30.R2A-3
+d17b_30_R2A_3 <- filter(tabgraf, isolado=='D17b.30.R2A-3')
+y_17b_R2A_3 <- d17b_30_R2A_3$sob
+fit_17b_R2A_3 <- nls(y_17b_R2A_3 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 100, n = 2))
+summary(fit_17b_R2A_3)
+ld10 <- append(ld10, summary(fit_17b_R2A_3)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_17b_R2A_3)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D17b.30.R2A-3', after=length(isolado))
+
+# Isolado D17b.30.R2A-4.2
+d17b_30_R2A_42 <- filter(tabgraf, isolado=='D17b.30.R2A-4.2')
+y_17b_R2A_42 <- d17b_30_R2A_42$sob
+fit_17b_R2A_42 <- nls(y_17b_R2A_42 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 250, n = 2))
+summary(fit_17b_R2A_42)
+ld10 <- append(ld10, summary(fit_17b_R2A_42)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_17b_R2A_42)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D17b.30.R2A-4.2', after=length(isolado))
+
+# Isolado D1c.30.010-2
+d1c_30_010_2 <- filter(tabgraf, isolado=='D1c.30.010-2')
+y_1c_010_2 <- d1c_30_010_2$sob
+fit_1c_010_2 <- nls(y_1c_010_2 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 100, n = 2))
+summary(fit_1c_010_2)
+ld10 <- append(ld10, summary(fit_1c_010_2)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_1c_010_2)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D1c.30.010-2', after=length(isolado))
+
+# Isolado D21.30.010-1
+d21_30_010_1 <- filter(tabgraf, isolado=='D21.30.010-1')
+y_21_010_1 <- d21_30_010_1$sob
+fit_21_010_1 <- nls(y_21_010_1 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 100, n = 2))
+summary(fit_21_010_1)
+ld10 <- append(ld10, summary(fit_21_010_1)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_21_010_1)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D21.30.010-1', after=length(isolado))
+
+# #### Isolado D4.30.R2A-3
+d4_30_R2A_3 <- filter(tabgraf, isolado=='D4.30.R2A-3')
+y_4_R2A_3 <- d4_30_R2A_3$sob
+fit_4_R2A_3 <- nls(y_4_R2A_3 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 300, n =  3))
+summary(fit_4_R2A_3)
+ld10 <- append(ld10, summary(fit_4_R2A_3)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_4_R2A_3)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D4.30.R2A-3', after=length(isolado))
+
+graf_fit<- ggplot(d4_30_R2A_3, aes(x=Dose, y=sob)) +
+    geom_point(shape=15, size=2)+   # Coloca as bolinhas
+    geom_errorbar(aes(ymin=sob-erro, ymax=sob+erro), width=0.15, size=.5)+    # Coloca as barras de erro
+    geom_smooth(method='nls', data=d4_30_R2A_3,
+                formula = y ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+                se=FALSE,
+                method.args = list(start = list(LD10 = 300, n = 3)))+
+    theme_bw()+   # Muda a aparência para o tema certo
+    xlab(bquote("Dose"~(J.m^-2)))+    # Coloca o nome do eixo x
+    ylab(bquote("Sobrevivência"~(N/N[0])))+    # Coloca o nome do eixo y
+    theme(
+      panel.grid.major = element_line(size=.25),   # Define largura da linha maior do grid interno no gráfico
+      panel.grid.minor = element_line(size=.1),   # Define largura da linha menor do grid interno do gráfico
+      legend.position='none')
+graf_fit
+
+
+
+
+# Isolado D5.30.010-5/2
+d5_30_010_52 <- filter(tabgraf, isolado=='D5.30.010-5/2')
+y_5_010_52 <- d5_30_010_52$sob
+fit_5_010_52 <- nls(y_5_010_52 ~ (1+f*x)/(1+(9+10*f*LD10)*exp(n*log(x/LD10))),
+			        start = list(LD10 = 170, n = 2))
+summary(fit_5_010_52)
+ld10 <- append(ld10, summary(fit_5_010_52)$parameters[1,1] , after=length(ld10))
+se_ld10 <- append(se_ld10, summary(fit_5_010_52)$parameters[1,2] , after=length(se_ld10))
+isolado <- append(isolado, 'D5.30.010-5/2', after=length(isolado))
